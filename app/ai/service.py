ac5681c99135a3397411bb366127f89edc134a9f
HobyGlyki <hobegoblin14.87@gmail.com>
2026-04-18 23:29:02 +0300

    change service.py

--- app/ai/service.py
+++ app/ai/service.py
@@ -6,7 +6,7 @@ import asyncio
 import time
 
 
-
+strs= {'biology': {'gender': 'мужчина', 'old': 733, 'race': 'орк'}, 'appearance': {'appearance': 'ходячий анекдот', 'is_nice': 'нет', 'mass': 240, 'height': 284}, 'health': {'heal': 'хрупкое создание', 'is_appearance': 'всё лицо — сплошной симптом'}, 'job': {'job': 'цепной пес режима', 'is_nice': 'Бесполезная', 'skill': 56, 'can_be_ability': 0, 'ability_type': 'абсолютная бездарность', 'ability ID': 10}, 'hobby': {'hobby': 'модельный сорт', 'is_nice': 'нет', 'is_job': 'нет'}, 'fact': {'is_positive': 'нет', 'is_inexpected': 'нет', 'chaos': 'Терминальное безумие'}, 'phobia': {'phobia': 'Легкий абсурд', 'is_nice': 'да'}, 'inventory_1': {'inventory': 'Странный заскок', 'is_job': 'да', 'is_nice': 'да'}, 'inventory_2': {'inventory': 'Легкий абсурд', 'is_job': 'нет', 'is_nice': 'нет'}, 'ability_1': {'ability name': 'депортация в бункер', 'is_chaotic': 'Запущенный маразм', 'ability ID': 9}, 'ability_2': {'ability name': 'регенерация тканей', 'is_chaotic': 'Легкий абсурд', 'ability ID': 1}}
 
 async def ai_response(data: dict) -> str:
     start_time = time.perf_counter() # Стартуем замер
@@ -21,7 +21,7 @@ async def ai_response(data: dict) -> str:
 
     client = AsyncClient()
     response = await client.chat(
-        model='llama3.2:3b',
+        model='qwen2.5:3b ',
         format='json', 
         messages=[
             {'role': 'system', 'content': system_content},
@@ -29,12 +29,11 @@ async def ai_response(data: dict) -> str:
         ],
         
         options={
-        "temperature": 0.4,    # Делает ответы более четкими и менее "размазанными"
-        "num_predict": 1024,   # Ограничиваем максимальную длину, чтобы она не уходила в бесконечные описания
-        "top_p": 0.9,          # Срезает маловероятные варианты слов, ускоряя выбор
-        "num_ctx": 2048,
-        "num_thread": 8
-    }
+            "temperature": 0.7,   # Чуть ниже, чтобы меньше галлюцинировал
+            "num_ctx": 3072,
+            "num_predict": 500,   # ЭТО ВАЖНО: 350 токенов за глаза хватит на 11 лаконичных полей
+            "repeat_penalty": 1.3 # Увеличим, чтобы он не повторял одни и те же слова
+        }
     )
     content = response['message']['content']
     end_time = time.perf_counter()
@@ -43,17 +42,26 @@ async def ai_response(data: dict) -> str:
     return content
 
 
-semaphore = asyncio.Semaphore(3)
+semaphore = asyncio.Semaphore(3)  # Ограничиваем до 5 одновременных запросов к AI
 
 async def safe_ai_response(data):
     async with semaphore:
         return await ai_response(data)
 
-async def main(cards: dict):
+async def main(cards: dict, count: int):
+    start_time = time.perf_counter() # Стартуем замер
     # Создаем список задач (корутин)
-    tasks = [safe_ai_response(cards) for _ in range(7)]
+    tasks = [safe_ai_response(cards) for _ in range(count)]
     all_results = await asyncio.gather(*tasks)
     # Запускаем всё ОДНОВРЕМЕННО
     # return_exceptions=True поможет, если одна генерация упадет, остальные выживут
+    end_time = time.perf_counter()
+    duration = end_time - start_time
     print(f"Сгенерировано персонажей: {len(all_results)}")
+    print(f"Время генерации: {duration:.2f} сек.")
     print(all_results)
+
+
+if __name__ == "__main__":
+    # Запускаем цикл ОДИН раз
+    asyncio.run(main(strs, 3))
